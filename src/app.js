2d98fb9a04a59ba876a0f97a71adb0c3db184c9d
Vedarth <vedarth.sharma@gmail.com>
2020-02-26 13:01:38 +0530

    More refactoring

--- src/app.js
+++ src/app.js
@@ -1,17 +1,15 @@
 const Express = require("express");
 const ExpressGraphQL = require("express-graphql");
+const { schema } = require("./graphql-api/design")
+const port = require('../config')['port']
 
 const app = Express();
-
-const { schema } = require("./graphql-api/design")
-
 app.use("/graphql", ExpressGraphQL({
     schema: schema,
     graphiql: true
 }));
-
-app.listen(3000, () => {
-    console.log("Listening at http://localhost:3000/graphql");
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}/graphql`);
 })
 
 module.exports = app;
